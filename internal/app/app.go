eb98f0f32a44e6fc42788d9a84be31a4829e815f
scmbr <romvasily@mail.ru>
2025-07-01 16:30:15 +0900

    feat: initialize db connection

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,7 @@ import (
 	"github.com/scmbr/test-task/internal/config"
 	delivery "github.com/scmbr/test-task/internal/delivery/http"
 	"github.com/scmbr/test-task/internal/server"
+	"github.com/scmbr/test-task/pkg/database"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,6 +13,17 @@ func Run(configsDir string) {
 	if err != nil {
 		logrus.Fatalf("error initializing configs: %s", err.Error())
 		return
+	}
+	db, err := database.NewPostgresDB(database.Config{
+		Host:     cfg.Postgres.Host,
+		Port:     cfg.Postgres.Port,
+		Username: cfg.Postgres.Username,
+		Password: cfg.Postgres.Password,
+		DBName:   cfg.Postgres.Name,
+		SSLMode:  cfg.Postgres.SSLMode,
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db:%s", err.Error())
 	}
 	srv := new(server.Server)
 	handlers := delivery.NewHandler()
