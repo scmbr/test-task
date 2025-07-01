0e907d6b6c1f694f62cd8f4f8ae62650195a56cf
scmbr <romvasily@mail.ru>
2025-07-01 22:23:55 +0900

    feat: add token DTO

---
+++ internal/dto/tokenDTO.go
@@ -0,0 +1,9 @@
+package dto
+
+type TokensResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+type GenerateTokensRequest struct {
+	GUID string `json:"guid" binding:"required,uuid"`
+}
