a870fa33d6be0585c968672039bfc8fb47471ee0
scmbr <romvasily@mail.ru>
2025-07-01 19:39:33 +0900

    feat: add refreshToken repository

---
+++ internal/repository/refresh_token_repo.go
@@ -0,0 +1,11 @@
+package repository
+
+import "gorm.io/gorm"
+
+type RefreshTokenRepo struct {
+	db *gorm.DB
+}
+
+func NewRefreshTokenRepo(db *gorm.DB) *RefreshTokenRepo {
+	return &RefreshTokenRepo{db: db}
+}
