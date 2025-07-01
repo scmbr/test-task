d11953e8b3aa310cc80b687bf0c291cdd8ab5320
scmbr <romvasily@mail.ru>
2025-07-01 21:39:14 +0900

    feat: add token manager

---
+++ pkg/auth/token_manager.go
@@ -0,0 +1,74 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type TokenManager interface {
+	NewJWT(role, userId string, ttl time.Duration) (string, error)
+	Parse(accessToken string) (*Claims, error)
+	NewRefreshToken() (string, error)
+}
+type Claims struct {
+	UserID string `json:"sub"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+type Manager struct {
+	signingKey string
+}
+
+func NewManager(signingKey string) (*Manager, error) {
+	if signingKey == "" {
+		return nil, errors.New("empty signing key")
+	}
+
+	return &Manager{signingKey: signingKey}, nil
+}
+
+func (m *Manager) NewJWT(role, userId string, ttl time.Duration) (string, error) {
+	claims := Claims{
+		UserID: userId,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(ttl)),
+			Subject:   userId,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	return token.SignedString([]byte(m.signingKey))
+}
+
+func (m *Manager) Parse(accessToken string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(m.signingKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token claims")
+	}
+
+	return claims, nil
+}
+
+func (m *Manager) NewRefreshToken() (string, error) {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(b), nil
+}
