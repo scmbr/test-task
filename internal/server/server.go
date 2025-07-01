308e496f49e71b728ba46face9e056bdbac08f2f
scmbr <romvasily@mail.ru>
2025-07-01 14:24:32 +0900

    fix: add host on server start

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,9 +10,9 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string, handler http.Handler) error {
+func (s *Server) Run(port string, host string, handler http.Handler) error {
 	s.httpServer = &http.Server{
-		Addr:           ":" + port,
+		Addr:           host + ":" + port,
 		Handler:        handler,
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
