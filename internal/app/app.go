7c381e68fecccd1601781823bd3a1998c6d16173
scmbr <romvasily@mail.ru>
2025-07-01 14:23:31 +0900

    feat: add config initialization

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,21 @@
 package app
 
 import (
+	"github.com/scmbr/test-task/internal/config"
 	delivery "github.com/scmbr/test-task/internal/delivery/http"
 	"github.com/scmbr/test-task/internal/server"
 	"github.com/sirupsen/logrus"
 )
 
 func Run(configsDir string) {
+	cfg, err := config.Init(configsDir)
+	if err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+		return
+	}
 	srv := new(server.Server)
 	handlers := delivery.NewHandler()
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(cfg.HTTP.Port, cfg.HTTP.Host, handlers.InitRoutes()); err != nil {
 		logrus.Fatalf("error occurred while starting http server: %s", err.Error())
 	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "time"
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type (
 	Config struct {
@@ -14,3 +19,30 @@ type (
 		MaxHeaderMegabytes int           `mapstructure:"maxHeaderBytes"`
 	}
 )
+
+func Init(configsDir string) (*Config, error) {
+	viper.AddConfigPath(configsDir)
+	viper.SetConfigName("main")
+	viper.SetConfigType("yml")
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+	var cfg Config
+	if err := unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	setFromEnv(&cfg)
+
+	return &cfg, nil
+}
+func unmarshal(cfg *Config) error {
+
+	if err := viper.UnmarshalKey("http", &cfg.HTTP); err != nil {
+		return err
+	}
+	return nil
+}
+func setFromEnv(cfg *Config) {
+	cfg.HTTP.Host = os.Getenv("HTTP_HOST")
+}
