54352a0e9f966c8c4a8025eecefcada73b09f444
scmbr <romvasily@mail.ru>
2025-07-01 22:41:40 +0900

    feat: add hasher,repo,tokenManager,service in app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,8 +3,12 @@ package app
 import (
 	"github.com/scmbr/test-task/internal/config"
 	delivery "github.com/scmbr/test-task/internal/delivery/http"
+	"github.com/scmbr/test-task/internal/repository"
 	"github.com/scmbr/test-task/internal/server"
+	"github.com/scmbr/test-task/internal/service"
+	"github.com/scmbr/test-task/pkg/auth"
 	"github.com/scmbr/test-task/pkg/database"
+	"github.com/scmbr/test-task/pkg/hasher"
 	"github.com/sirupsen/logrus"
 )
 
@@ -25,8 +29,22 @@ func Run(configsDir string) {
 	if err != nil {
 		logrus.Fatalf("failed to initialize db:%s", err.Error())
 	}
+	hasher := hasher.NewHasher(cfg.Hasher.Cost)
+	repositories := repository.NewRepository(db)
+	tokenManager, err := auth.NewManager(cfg.Auth.SigningKey)
+	if err != nil {
+		logrus.Fatalf("error initializing token manager: %s", err.Error())
+		return
+	}
+	services := service.NewServices(service.Deps{
+		Repos:           repositories,
+		Hasher:          *hasher,
+		TokenManager:    tokenManager,
+		AccessTokenTTL:  cfg.Auth.AccessTokenTTL,
+		RefreshTokenTTL: cfg.Auth.RefreshTokenTTL,
+	})
 	srv := new(server.Server)
-	handlers := delivery.NewHandler()
+	handlers := delivery.NewHandler(services)
 	if err := srv.Run(cfg.HTTP.Port, cfg.HTTP.Host, handlers.InitRoutes()); err != nil {
 		logrus.Fatalf("error occurred while starting http server: %s", err.Error())
 	}
