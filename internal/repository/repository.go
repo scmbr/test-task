b639f76a8a6989036cc43ec95f20b2fe4e91f9ec
scmbr <romvasily@mail.ru>
2025-07-02 16:19:10 +0900

    chore: remove unused interfaces

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,23 +1,21 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"time"
 
-type User interface {
-}
+	"gorm.io/gorm"
+)
+
 type RefreshToken interface {
+	SaveRefreshToken(guid, hashedToken, userAgent, ip string, refreshTokenTTL time.Duration) error
 }
-type WebhookLog interface {
-}
+
 type Repository struct {
-	User
 	RefreshToken
-	WebhookLog
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		User:         NewUserRepo(db),
 		RefreshToken: NewRefreshTokenRepo(db),
-		WebhookLog:   NewWebhookLogRepo(db),
 	}
 }
