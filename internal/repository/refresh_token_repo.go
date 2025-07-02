7fe383ebdfaf5d8f43830ad935b136a1915025ab
scmbr <romvasily@mail.ru>
2025-07-02 16:50:47 +0900

    feat: add SaveRefreshToken method

--- internal/repository/refresh_token_repo.go
+++ internal/repository/refresh_token_repo.go
@@ -1,6 +1,12 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"time"
+
+	"github.com/scmbr/test-task/internal/models"
+	"gorm.io/gorm"
+)
 
 type RefreshTokenRepo struct {
 	db *gorm.DB
@@ -8,4 +14,18 @@ type RefreshTokenRepo struct {
 
 func NewRefreshTokenRepo(db *gorm.DB) *RefreshTokenRepo {
 	return &RefreshTokenRepo{db: db}
+}
+func (r *RefreshTokenRepo) SaveRefreshToken(guid, hashedToken, userAgent, ip string, refreshTokenTTL time.Duration) error {
+	token := models.RefreshToken{
+		UserGUID:  guid,
+		TokenHash: hashedToken,
+		UserAgent: userAgent,
+		IP:        ip,
+		ExpiresAt: time.Now().Add(refreshTokenTTL),
+	}
+	if err := r.db.Create(&token).Error; err != nil {
+		return fmt.Errorf("failed to save refresh token: %w", err)
+	}
+
+	return nil
 }
