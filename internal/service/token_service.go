85837f3884229090f02241b8b7608a761859eb68
scmbr <romvasily@mail.ru>
2025-07-03 22:21:44 +0900

    feat: add RefreshTokenPair method in service

--- internal/service/token_service.go
+++ internal/service/token_service.go
@@ -1,28 +1,35 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
+	"github.com/scmbr/test-task/internal/dto"
+	"github.com/scmbr/test-task/internal/notifier"
 	"github.com/scmbr/test-task/internal/repository"
 	"github.com/scmbr/test-task/pkg/auth"
 	"github.com/scmbr/test-task/pkg/hasher"
+	"github.com/sirupsen/logrus"
 )
 
 type TokenService struct {
-	repo            repository.RefreshToken
-	hasher          hasher.Hasher
-	accessTokenTTL  time.Duration
-	refreshTokenTTL time.Duration
-	tokenManager    auth.TokenManager
+	repo             repository.RefreshToken
+	hasher           hasher.Hasher
+	accessTokenTTL   time.Duration
+	refreshTokenTTL  time.Duration
+	tokenManager     auth.TokenManager
+	ipChangeNotifier *notifier.IPNotifier
 }
 
-func NewTokenService(repo repository.RefreshToken, hasher hasher.Hasher, accessTTL, refreshTTL time.Duration, tokenManager auth.TokenManager) *TokenService {
+func NewTokenService(repo repository.RefreshToken, hasher hasher.Hasher, accessTTL, refreshTTL time.Duration, tokenManager auth.TokenManager, ipChangeNotifier *notifier.IPNotifier) *TokenService {
 	return &TokenService{
-		repo:            repo,
-		hasher:          hasher,
-		tokenManager:    tokenManager,
-		accessTokenTTL:  accessTTL,
-		refreshTokenTTL: refreshTTL,
+		repo:             repo,
+		hasher:           hasher,
+		tokenManager:     tokenManager,
+		accessTokenTTL:   accessTTL,
+		refreshTokenTTL:  refreshTTL,
+		ipChangeNotifier: ipChangeNotifier,
 	}
 }
 
@@ -43,4 +50,52 @@ func (s *TokenService) GenerateAndSaveRefreshToken(guid, userAgent, ip string) (
 		return "", err
 	}
 	return rawToken, nil
+}
+func (s *TokenService) RefreshTokenPair(refreshToken, accessToken, userAgent, clientIP string) (*dto.TokensResponse, error) {
+	accessClaims, err := s.tokenManager.Parse(accessToken)
+	if err != nil {
+		return nil, fmt.Errorf("invalid access token: %w", err)
+	}
+	refreshTokenHash, err := s.hasher.Hash(refreshToken)
+	if err != nil {
+		return &dto.TokensResponse{}, err
+	}
+	refreshTokenModel, err := s.repo.ValidateRefreshToken(refreshTokenHash)
+	if err != nil {
+		return nil, fmt.Errorf("invalid refresh token: %w", err)
+	}
+
+	if accessClaims.UserGUID != refreshTokenModel.UserGUID {
+		return nil, errors.New("token pair mismatch")
+	}
+
+	if refreshTokenModel.UserAgent != userAgent {
+		if err := s.repo.DeleteAllUserRefreshTokens(refreshTokenModel.UserGUID); err != nil {
+			logrus.Errorf("failed to delete tokens: %v", err)
+		}
+		return nil, errors.New("user agent changed - all user's tokens deleted")
+	}
+
+	if refreshTokenModel.IP != clientIP {
+		go s.ipChangeNotifier.NotifyChange(refreshTokenModel.UserGUID, refreshTokenModel.IP, clientIP)
+	}
+
+	newAccess, err := s.tokenManager.NewJWT(refreshTokenModel.UserGUID, s.accessTokenTTL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate access token: %w", err)
+	}
+
+	newRefresh, err := s.tokenManager.NewRefreshToken()
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate refresh token: %w", err)
+	}
+
+	if err := s.repo.DeleteRefreshToken(refreshTokenHash); err != nil {
+		logrus.Errorf("failed to delete old refresh token: %v", err)
+	}
+
+	return &dto.TokensResponse{
+		AccessToken:  newAccess,
+		RefreshToken: newRefresh,
+	}, nil
 }
