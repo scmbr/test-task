2a351d5624cecdebcd6feab2aa1d2dfbb40ba551
scmbr <romvasily@mail.ru>
2025-07-03 17:37:15 +0900

    feat: add RefreshTokenData DTO

--- internal/dto/tokenDTO.go
+++ internal/dto/tokenDTO.go
@@ -11,3 +11,8 @@ type RefreshTokenRequest struct {
 	RefreshToken string `json:"refresh_token" binding:"required"`
 	AccessToken  string `json:"access_token" binding:"required"`
 }
+type RefreshTokenData struct {
+	UserGUID  string
+	UserAgent string
+	IP        string
+}
