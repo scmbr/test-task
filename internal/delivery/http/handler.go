117489721ed7ac5a4f5e0868077b0ecf3a8ffbb9
scmbr <romvasily@mail.ru>
2025-07-05 06:09:35 +0900

    feat: add endpoint for swagger

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/scmbr/test-task/internal/service"
 	"github.com/scmbr/test-task/pkg/auth"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type Handler struct {
@@ -16,6 +18,7 @@ func NewHandler(service *service.Service, tokenManager auth.TokenManager) *Handl
 }
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	api := router.Group("/api")
 	{
 		auth := api.Group("/auth")
