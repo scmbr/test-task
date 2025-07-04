9c8674871c7e6b1104d078cf16b96b72247ca111
scmbr <romvasily@mail.ru>
2025-07-05 06:08:57 +0900

    feat: add swagger documentation for endpoints

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/scmbr/test-task/internal/app"
+import (
+	_ "github.com/scmbr/test-task/docs"
+	"github.com/scmbr/test-task/internal/app"
+)
 
 const configsDir = "configs"
 
+// @title Authorization API test task
+// @version 1.0
+// @description Реализация тестового задания medods
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @host localhost:8080
+// @BasePath /
 func main() {
 	app.Run(configsDir)
 }
--- internal/delivery/http/auth.go
+++ internal/delivery/http/auth.go
@@ -7,6 +7,16 @@ import (
 	"github.com/scmbr/test-task/internal/dto"
 )
 
+// @Summary Генерация пары токенов
+// @Description Создает access и refresh токены для указанного пользователя
+// @Tags Auth
+// @Accept json
+// @Produce json
+// @Param request body dto.GenerateTokensRequest true "User GUID"
+// @Success 200 {object} dto.TokensResponse
+// @Failure 400 {object} dto.ErrorResponse
+// @Failure 500 {object} dto.ErrorResponse
+// @Router /api/auth/token [post]
 func (h *Handler) generateTokens(c *gin.Context) {
 	var req dto.GenerateTokensRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -31,6 +41,17 @@ func (h *Handler) generateTokens(c *gin.Context) {
 		RefreshToken: refreshToken,
 	})
 }
+
+// @Summary Обновление пары токенов
+// @Description Обновляет access и refresh токены парой токенов, которая была выдана вместе
+// @Tags Auth
+// @Accept json
+// @Produce json
+// @Param request body dto.RefreshTokenRequest true "Tokens"
+// @Success 200 {object} dto.TokensResponse
+// @Failure 401 {object} dto.ErrorResponse
+// @Failure 500 {object} dto.ErrorResponse
+// @Router /api/auth/refresh [post]
 func (h *Handler) refreshTokens(c *gin.Context) {
 	var req dto.RefreshTokenRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -49,6 +70,15 @@ func (h *Handler) refreshTokens(c *gin.Context) {
 
 	c.JSON(http.StatusOK, dto.TokensResponse{AccessToken: newTokens.AccessToken, RefreshToken: newTokens.RefreshToken})
 }
+
+// @Summary Получение GUID текущего пользователя
+// @Description Возвращает GUID авторизованного пользователя
+// @Tags User
+// @Security ApiKeyAuth
+// @Produce json
+// @Success 200 {object} dto.UserGUIDResponse
+// @Failure 401 {object} dto.ErrorResponse
+// @Router /api/auth/user [get]
 func (h *Handler) getCurrentUserGUID(c *gin.Context) {
 	userGUID, exists := c.Get("userGuid")
 	if !exists {
@@ -64,6 +94,18 @@ func (h *Handler) getCurrentUserGUID(c *gin.Context) {
 
 	c.JSON(http.StatusOK, dto.UserGUIDResponse{UserGUID: userGuid})
 }
+
+// @Summary Выход из системы
+// @Description Удаляет все refresh-токены пользователя
+// @Tags Auth
+// @Security ApiKeyAuth
+// @Accept json
+// @Produce json
+// @Param access_token body dto.LogoutRequest true "Access Token"
+// @Success 200 {object} dto.MessageResponse
+// @Failure 400 {object} dto.ErrorResponse
+// @Failure 500 {object} dto.ErrorResponse
+// @Router /api/auth/logout [post]
 func (h *Handler) logOut(c *gin.Context) {
 	var req dto.LogoutRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
